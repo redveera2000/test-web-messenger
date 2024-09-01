2a8483fafece2bbece7367d1ef4ed7c344dad854
redveera2000 <veeradinesh219@gmail.com>
2024-09-01 10:50:00 +0000

    Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -21,7 +21,7 @@ const Header = (props) => {
   return(
     <header className="header">
         <div style={{display: 'flex'}}>
-          <div className="logo">Web Messenger</div>
+          <div className="logo">Instagram</div>
             
             {
               !auth.authenticated ? 
